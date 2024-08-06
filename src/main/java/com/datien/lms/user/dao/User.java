e89e4dc69317053b0faf61f922e5fbee20156093
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2024-08-06 23:49:42 +0700

    application.yml congif

--- src/main/java/com/datien/lms/user/dao/User.java
+++ src/main/java/com/datien/lms/user/dao/User.java
@@ -1,4 +1,23 @@
 package com.datien.lms.user.dao;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tbl_user")
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
 }
