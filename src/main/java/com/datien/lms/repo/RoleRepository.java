df7ea23c031007bcca33a400d879ecb1e5d5f9de
Hải Nguyễn <ntdat14092003@gmail.com>
2024-08-15 10:37:01 +0700

    fix merge bug done!

--- src/main/java/com/datien/lms/repo/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.datien.lms.repo;
-
-import com.datien.lms.dao.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
