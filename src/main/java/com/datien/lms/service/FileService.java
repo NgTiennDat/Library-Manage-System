836dcf8b85ef62702017a535f7c040c1595d513a
Nguyễn Tiến Đạt <126660251+NgTiennDat@users.noreply.github.com>
2024-09-04 15:51:01 +0700

    update file config

--- src/main/java/com/datien/lms/service/FileService.java
+++ src/main/java/com/datien/lms/service/FileService.java
@@ -6,6 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static java.io.File.separator;
 
 @Service
@@ -13,13 +18,55 @@ import static java.io.File.separator;
 @RequiredArgsConstructor
 public class FileService {
 
+    private String fileUploadPath;
+
     private String saveFile(
-            @Nonnull MultipartFile file,
+            @Nonnull MultipartFile sourceFile,
             @Nonnull Long bookId,
             @Nonnull Long userId
     ) {
         final String fileUploadSubPath = "users" + separator + userId;
+        return uploadFile(sourceFile, fileUploadSubPath);
+    }
+
+    private String uploadFile(
+            @Nonnull MultipartFile sourceFile,
+            @Nonnull String fileUploadSubPath
+    ) {
+        final String finalUploadPath = fileUploadPath + separator + fileUploadSubPath;
+
+        File targetFolder = new File(fileUploadSubPath);
+
+        if(!targetFolder.exists()) {
+            boolean folderCreated = targetFolder.mkdirs();
+            if(!folderCreated) {
+                log.warn("Could not create folder {}", targetFolder.getAbsolutePath());
+                return null;
+            }
+        }
+
+        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        String targetFilePath = finalUploadPath + separator + System.currentTimeMillis() + "." + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File saved to: {}", targetFilePath);
+            return targetFilePath;
+        } catch (Exception ex) {
+            log.error("File was not found", ex);
+        }
         return null;
+    }
+
+    private String getFileExtension(String originalFilename) {
+        if(originalFilename == null) {
+            return "";
+        }
+        if(originalFilename.lastIndexOf(".") != -1) {
+            return "";
+        }
+        return originalFilename.substring(originalFilename.lastIndexOf(".") + 1).toLowerCase();
     }
 
 }
