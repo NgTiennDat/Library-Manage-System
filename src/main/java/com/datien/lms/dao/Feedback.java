4074de85e1a341b647ee2f8aa532f413103af83c
Nguyễn Tiến Đạt <126660251+NgTiennDat@users.noreply.github.com>
2024-09-04 10:43:00 +0700

    add Feedback attribute

--- src/main/java/com/datien/lms/dao/Feedback.java
+++ src/main/java/com/datien/lms/dao/Feedback.java
@@ -1,4 +1,13 @@
 package com.datien.lms.dao;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class Feedback {
+    private Double note;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "book-id")
+    private Book book;
 }
