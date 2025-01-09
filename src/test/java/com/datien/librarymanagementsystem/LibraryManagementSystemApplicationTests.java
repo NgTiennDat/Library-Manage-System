95717e5399e9cb5a586b867ead7fa5208cff8b1c
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2025-01-09 15:24:23 +0700

    update: fix test

--- src/test/java/com/datien/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
+++ src/test/java/com/datien/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -1,9 +1,10 @@
 package com.datien.librarymanagementsystem;
 
+import com.datien.lms.LmsApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LmsApplication.class)
 class LibraryManagementSystemApplicationTests {
 
     @Test
